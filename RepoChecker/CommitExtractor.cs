465daa5e4f7f32113b36a96d9d3500ce3607f7a2
unknown <16364092+dolot3@users.noreply.github.com>
2020-07-21 11:27:21 -0500

    Added interface

---
+++ RepoChecker/CommitExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RepoChecker
+{
+    public class CommitExtractor : ICommitExtractor
+    {
+        public string RepoName { get; set; }
+
+        public string GetCommitsRaw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ RepoChecker/ICommitExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace RepoChecker
+{
+    interface ICommitExtractor
+    {
+
+        string RepoName { get; set; }
+
+        string GetCommitsRaw();
+
+    }
+}
